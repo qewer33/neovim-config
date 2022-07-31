b1c56e25993f1b723f71a0fe5b320868de48a4d9
qewer33 <yerdemergul@gmail.com>
2022-07-31 21:56:30 +0300

    Organize plugins and One Dark theme

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,19 @@
 return require('packer').startup(function()
     use 'wbthomason/packer.nvim'                -- Package manager
+
+    -- Themes
+    use 'navarasu/onedark.nvim'                 -- One Dark theme
+    use "ellisonleao/gruvbox.nvim"              -- Gruvbox theme
+
+    -- Appereance
     use 'nvim-treesitter/nvim-treesitter'       -- Treesitter for better syntax hi
+    use "lukas-reineke/indent-blankline.nvim"   -- Indent guides
+    use {                                       -- Lualine for the statusline
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+
+    -- Editing & Features
     use 'neovim/nvim-lspconfig'                 -- Configurations for Nvim LSP
     use 'hrsh7th/nvim-cmp'                      -- Autocompletion
     use 'hrsh7th/cmp-nvim-lsp'                  -- LSP source for nvim-cmp
@@ -8,31 +21,29 @@ return require('packer').startup(function()
     use 'L3MON4D3/LuaSnip'                      -- Snippets plugin
     use 'hrsh7th/vim-vsnip'                     -- Snippets plugin (needed for nvim.cmp)
     use 'hrsh8th/vim-vsnip-integ'               -- Snippets plugin integ (needed for nvim.cmp)
-    use "ellisonleao/gruvbox.nvim"              -- Gruvbox theme
     use 'mhartington/formatter.nvim'            -- Autoformatter
 	use "windwp/nvim-autopairs"                 -- Bracket auto close
     use 'cappyzawa/trim.nvim'                   -- Trim whitespaces on save
-    use 'simrat39/rust-tools.nvim'              -- Rust
-    use 'nvim-lua/plenary.nvim'
     use 'mfussenegger/nvim-dap'
-    use "lukas-reineke/indent-blankline.nvim"   -- Indent guides
+
+    -- Interface Features
+    use 'nvim-lua/plenary.nvim'
     use 'wfxr/minimap.vim'                      -- Minimap
-    use 'rust-lang/rust.vim'
-    use {                                       -- Lualine for the statusline
-        'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    }
     use {                                       -- Telescope for file navigation and other stuff
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
         requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use {                                       -- Tab support
+      'romgrk/barbar.nvim',
+      requires = {'kyazdani42/nvim-web-devicons'}
     }
     use {                                       -- File tree
       'kyazdani42/nvim-tree.lua',
       requires = { 'kyazdani42/nvim-web-devicons'},
       tag = 'nightly'
     }
-    use {                                       -- Tab support
-      'romgrk/barbar.nvim',
-      requires = {'kyazdani42/nvim-web-devicons'}
-    }
+
+    -- Language Specific
+    use 'simrat39/rust-tools.nvim'              -- Rust
+    use 'rust-lang/rust.vim'
 end)
